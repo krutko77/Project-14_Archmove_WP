b6ea5f8b4aeefbab23364492c6461cf1e92304df
krutko77 <70036691+krutko77@users.noreply.github.com>
2022-01-25 00:58:47 +0300

    burger menu js

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,6 +1,14 @@
-// Подключение функционала "Чертогов Фрилансера"
-import { isMobile } from "./functions.js";
-// Подключение списка активных модулей
-import { flsModules } from "./modules.js";
+// // Подключение функционала "Чертогов Фрилансера"
+// import { isMobile } from "./functions.js";
+// // Подключение списка активных модулей
+// import { flsModules } from "./modules.js";
 
 
+const iconMenu = document.querySelector('.header__icon-menu');
+   if (iconMenu) {
+      const headerMenu = document.querySelector('.header__menu');
+      iconMenu.addEventListener("click", function(e){
+         iconMenu.classList.toggle('_active');
+         headerMenu.classList.toggle('_active');
+      });
+   }
